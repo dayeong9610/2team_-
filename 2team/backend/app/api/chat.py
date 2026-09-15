46dfbb00d8bdff3a5ad63a9d4b7844663336b091
김티모 <xvxv8533@gmail.com>
2026-09-15 11:54:06 +0900

    수정

--- 2team/backend/app/api/chat.py
+++ 2team/backend/app/api/chat.py
@@ -9,37 +9,17 @@ from app.core.scenario_engine import (
     get_stage
 )
 
+from app.services.ai_service import evaluate_response
 
 router = APIRouter(
     tags=["Chat"]
 )
 
-#임시로 처리
-def mock_ai_response(
-    stage_id: str,
-    message: str
-):
-
-    return {
-        "npc_response":
-        "왜? 다들 하는데 한 번 정도는 괜찮지 않아?",
-
-        "feedback":
-        "답변을 잘 표현했어요.",
-
-        "scores": {
-            "risk_awareness": 1,
-            "refusal": 2,
-            "help_request": 0
-        }
-    }
-
-
 @router.post(
     "/chat",
     response_model=ChatResponse
 )
-def chat(
+async def chat(
     request: ChatRequest
 ):
 
@@ -54,17 +34,20 @@ def chat(
             status_code=404,
             detail="Stage not found"
         )
-#Mock에서 실제 AI로 변경할 때 나중교체해야됨 아래껀 삭제
-# ai_result = await evaluate_response(
-#     episode_id=request.episode_id,
-#     stage_id=request.stage_id,
-#     user_message=request.message,
-#     stage_data=stage
-# )
-    ai_result = mock_ai_response(
-        request.stage_id,
-        request.message
+
+    ai_result = await evaluate_response(
+        episode_id=request.episode_id,
+        stage_id=request.stage_id,
+        user_message=request.message,
+        stage_data=stage
     )
+
+    # 여기 추가
+    update_score(
+        request.session_id,
+        ai_result["scores"]
+    )
+
 
     next_stage = stage.get(
         "next_stage"
